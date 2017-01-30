41c44caf8a1714da038a6a645155392ab39e3140
Chris Sullivan <sullivan@nscl.msu.edu>
2017-01-29 19:31:45 -0500

    Now pass pyneat Population through to the QStandardItem via it's setData(QVariant) method. This then allows the selection callback to utilize the population upon selection in the treeview. When a new generation is selected, information about that generation is displayed in a new QListWidget that is added to the QStackedWidget information panel.

--- pygui/main_window.py
+++ pygui/main_window.py
@@ -4,6 +4,8 @@ from PyQt4.QtGui import *
 import numpy as np
 import itertools
 import sys
+sys.path.append(os.path.join(os.path.dirname(__file__),'.'))
+from pyneat_items import *
 sys.path.append(os.path.join(os.path.dirname(__file__),'../lib'))
 import pyneat
 
@@ -27,12 +29,12 @@ class MainWindow(QMainWindow):
         self.ui.info_layout.addWidget(self.info_stack)
 
         # set up data model
-        standard_model = QStandardItemModel()
+        self.standard_model = QStandardItemModel()
         self.generation_parent = QStandardItem("Generations")
-        standard_model.invisibleRootItem().appendRow(self.generation_parent)
+        self.standard_model.invisibleRootItem().appendRow(self.generation_parent)
 
         # register model with the ui tree view
-        self.ui.tree_view.setModel(standard_model)
+        self.ui.tree_view.setModel(self.standard_model)
 
         # initialize pyneat neat_xor example
         self.prob = pyneat.Probabilities()
@@ -49,20 +51,39 @@ class MainWindow(QMainWindow):
     @QtCore.pyqtSlot('QItemSelection, QItemSelection')
     def tree_selection(self, selected, deselected):
         """ The callback made when the selectionChanged
-        signal is emitted from the tree_view class."""
+        signal is emitted from the tree_view class. """
+        # currently creates a new widget everytime a seclection
+        # is made, which should be changed to something like a
+        # dictionary lookup for the needed widget given a
+        # selection index (possible key, QIndex from selected.indexes())
 
-        info_list = QListWidget()
-        item = QListWidgetItem("Number of species = {}".format(int(np.random.uniform(0,100))))
-        info_list.addItem(item)
-        widget_index = self.info_stack.count()
-        self.info_stack.addWidget(info_list)
-        self.info_stack.setCurrentIndex(widget_index)
-        #print(selected)
+        generation = None
+        selected_item = None
+        for index in selected.indexes():
+            item = self.standard_model.itemFromIndex(index)
+            if item.data() is None:
+                continue
+            generation = item.data()
+            selected_item = item
+
+        if generation is not None:
+            info_list = QListWidget()
+            root = QListWidgetItem(selected_item.accessibleText())
+            new_list_item = QListWidgetItem("Number of Organisms = {}".format(len(generation.organisms)))
+            info_list.addItem(root)
+            info_list.addItem(new_list_item)
+            widget_index = self.info_stack.count()
+            self.info_stack.addWidget(info_list)
+            self.info_stack.setCurrentIndex(widget_index)
 
 
     def advance_one_gen(self):
         self.generations.append(self.generations[-1].Reproduce(xor))
-        item = QStandardItem("Gen. {}".format(len(self.generations)-1))
+        text = "Gen. {}".format(len(self.generations)-1)
+        item = QStandardItem(text)
+        item.setAccessibleText(text)
+        item.setData(self.generations[-1])
+        #item = GenerationItem("Gen. {}".format(len(self.generations)-1), self.generations[-1])
         self.generation_parent.appendRow(item)
 
     def advance_ten_gen(self):
