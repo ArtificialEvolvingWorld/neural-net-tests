fe16676d8ffc97d4ad0686fb7068176a9ebf4223
Eldritch Cheese <eldritch.cheese@gmail.com>
2017-02-08 20:26:31 -0500

    Added histograms for population/species diagnostics.

--- pygui/population_diagnostics.py
+++ pygui/population_diagnostics.py
@@ -2,8 +2,14 @@ import os
 
 from PyQt4 import uic, QtCore, QtGui
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+
 (Ui_Widget, QWidget) = uic.loadUiType(
     os.path.join(os.path.dirname(__file__),'population_diagnostics.ui'))
+
+from pygui.util import fill_placeholder
 
 
 class PopulationDiagnostics(QWidget):
@@ -14,6 +20,8 @@ class PopulationDiagnostics(QWidget):
 
         self.population = population
         self.load_values()
+        self.setup_figure()
+        self.make_plot()
 
     def load_values(self):
         num_species = len(self.population.species)
@@ -25,3 +33,27 @@ class PopulationDiagnostics(QWidget):
         self.ui.num_organisms.setText(str(num_organisms))
         self.ui.num_species.setText(str(num_species))
         self.ui.best_fitness.setText(str(best_fitness))
+
+    def setup_figure(self):
+        bg = self.palette().window().color()
+        color = (bg.redF(), bg.greenF(), bg.blueF())
+        self.figure = plt.figure(edgecolor=color, facecolor=color)
+        self.canvas = FigureCanvas(self.figure)
+        toolbar = NavigationToolbar(self.canvas, self)
+        toolbar.pan()
+        toolbar.hide()
+
+        fill_placeholder(self.ui.plot_holder, self.canvas)
+
+    def make_plot(self):
+        self.figure.clear()
+
+        axes = self.figure.add_subplot(1,1,1)
+        axes.hist([org.fitness
+                   for spec in self.population.species
+                   for org in spec.organisms])
+        axes.set_xlabel('Fitness')
+        axes.set_ylabel('Count')
+        self.figure.tight_layout()
+
+        self.canvas.draw()
--- pygui/species_diagnostics.py
+++ pygui/species_diagnostics.py
@@ -2,8 +2,14 @@ import os
 
 from PyQt4 import uic, QtCore, QtGui
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+
 (Ui_Widget, QWidget) = uic.loadUiType(
     os.path.join(os.path.dirname(__file__),'species_diagnostics.ui'))
+
+from pygui.util import fill_placeholder
 
 
 class SpeciesDiagnostics(QWidget):
@@ -14,9 +20,33 @@ class SpeciesDiagnostics(QWidget):
 
         self.species = species
         self.load_values()
+        self.setup_figure()
+        self.make_plot()
 
     def load_values(self):
         num_organisms = len(self.species.organisms)
         self.ui.num_organisms.setText(str(num_organisms))
         self.ui.best_fitness.setText(str(self.species.best_fitness))
         self.ui.age.setText(str(self.species.age))
+
+    def setup_figure(self):
+        bg = self.palette().window().color()
+        color = (bg.redF(), bg.greenF(), bg.blueF())
+        self.figure = plt.figure(edgecolor=color, facecolor=color)
+        self.canvas = FigureCanvas(self.figure)
+        toolbar = NavigationToolbar(self.canvas, self)
+        toolbar.pan()
+        toolbar.hide()
+
+        fill_placeholder(self.ui.plot_holder, self.canvas)
+
+    def make_plot(self):
+        self.figure.clear()
+
+        axes = self.figure.add_subplot(1,1,1)
+        axes.hist([org.fitness for org in self.species.organisms])
+        axes.set_xlabel('Fitness')
+        axes.set_ylabel('Count')
+        self.figure.tight_layout()
+
+        self.canvas.draw()
