cd80c0f7aec727267d5bbf578639cfa072e80f0c
Eldritch Cheese <eldritch.cheese@gmail.com>
2017-02-26 18:21:52 -0500

    Clean closing of widgets when replacing the diagnostics window.

--- pygui/util.py
+++ pygui/util.py
@@ -1,8 +1,14 @@
 from PyQt4 import QtGui
 
 def fill_placeholder(placeholder, widget):
-    if placeholder.layout() is not None:
-        QtGui.QWidget().setLayout(placeholder.layout())
-    layout = QtGui.QVBoxLayout(placeholder)
+    if placeholder.layout() is None:
+        placeholder.setLayout(QtGui.QVBoxLayout(placeholder))
+
+    layout = placeholder.layout()
+
+    while layout.count():
+        widget_to_delete = layout.itemAt(0).widget()
+        layout.removeWidget(widget_to_delete)
+        widget_to_delete.close()
+
     layout.addWidget(widget)
-    placeholder.setLayout(layout)
