32ca3c75e93bb32d8acdff36ee22ab23332ec17a
Eldritch Cheese <eldritch.cheese@gmail.com>
2017-11-19 16:02:10 -0800

    Added counter for total amount of data transferred.

--- web-serve/entendre.js
+++ web-serve/entendre.js
@@ -1,6 +1,8 @@
 var connection = null;
 var message_sent_box = document.getElementById("text-message-sent");
 var message_received_box = document.getElementById("text-message-received");
+var total_bytes_sent = 0;
+var total_bytes_received = 0;
 
 var all_generation_info = {};
 
@@ -21,6 +23,9 @@ function reconnect() {
 function send_message(obj) {
     var message = JSON.stringify(obj);
     connection.send(message);
+
+    total_bytes_sent += message.length;
+    document.getElementById('total-bytes-sent').innerHTML = format_bytes(total_bytes_sent);
 
     message_sent_box.innerHTML = message + "\n" + message_sent_box.innerHTML;
 }
@@ -54,6 +59,9 @@ function on_receive_message(event) {
     var message = event.data;
     message_received_box.innerHTML = message + "\n" + message_received_box.innerHTML;
 
+    total_bytes_received += message.length;
+    document.getElementById('total-bytes-received').innerHTML = format_bytes(total_bytes_received);
+
     var parsed = JSON.parse(message);
     if(parsed.hasOwnProperty('authenticated')) {
         send_message({"overview_requested": true});
@@ -68,6 +76,18 @@ function on_receive_message(event) {
 
     if(parsed.hasOwnProperty('generation_information')) {
         display_new_generations(parsed['generation_information']);
+    }
+}
+
+function format_bytes(bytes) {
+    if(bytes < Math.pow(1024,1)) {
+        return bytes + ' bytes';
+    } else if (bytes < Math.pow(1024,2)) {
+        return (bytes/1024).toFixed(1) + ' kB';
+    } else if (bytes < Math.pow(1024,3)) {
+        return (bytes/1024/1024).toFixed(2) + ' MB';
+    } else {
+        return (bytes/1024/1024/1024).toFixed(3) + ' GB';
     }
 }
 
